ac0db2ca6902a994fa6a4642769322e612373252
Sharmaz <irae45@gmail.com>
2015-10-20 15:37:08 -0500

    Agregado caché en LocalStorage

--- public/util/marvel-api.js
+++ public/util/marvel-api.js
@@ -10,24 +10,57 @@ var MarvelApi = (function () {
 	function MarvelApi(key) {
 		_classCallCheck(this, MarvelApi);
 
+		// constructor se llama cuando iniciamos nuestra clase con new MarvelApi()
 		this.key = key;
 		this.baseUrl = 'http://gateway.marvel.com/v1/public/';
+		//asignar propiedades a this
 	}
 
 	_createClass(MarvelApi, [{
 		key: 'findSeries',
 		value: function findSeries(title) {
 			var url = this.baseUrl + 'series?title=' + title + '&apikey=' + this.key;
-			return Promise.resolve($.get(url)).then(function (res) {
-				return res.data.results[0];
-			});
+			//construimos la url que se necesita para obtener datos de los avengers
+
+			//siempre pide datos a la API de Marvel
+			if (localStorage[url]) {
+				var datos = localStorage[url]; //traerDeCache
+				datos = JSON.parse(datos);
+				console.log('hola cache');
+				return Promise.resolve(datos);
+			} else {
+				return Promise.resolve($.get(url))
+				// hace que la petición de jQuery se vuelva una Promise
+				.then(function (res) {
+					//guardarResultadoEnCache()
+					var datos = res.data.results[0];
+					datos = JSON.stringify(datos);
+					localStorage[url] = datos;
+					//regresamos una nueva promesa con el
+					//primer resultado de acuerdo a lo que nos regresa marvel
+					return Promise.resolve(datos);
+				});
+			}
 		}
 	}, {
 		key: 'getResourceURI',
 		value: function getResourceURI(resourceURI) {
+			// este metodo es muy similar al de arriba
+			//¿Podrias crear un metodo interno al que llamen estos dos?
 			var url = resourceURI + '?apikey=' + this.key;
+
+			if (localStorage[url]) {
+				var datos = localStorage[url];
+				datos = JSON.parse(datos);
+				console.log('Hola Cache');
+				return Promise.resolve(datos);
+			}
+
 			return Promise.resolve($.get(url)).then(function (res) {
-				return res.data.results[0];
+				var datos = res.data.results[0];
+				datos = JSON.stringify(datos);
+				localStorage[url] = datos;
+				return Promise.resolve(datos);
 			});
 		}
 	}]);
