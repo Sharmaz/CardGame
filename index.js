4d807830242b5cb37c56fa3f655d9c9e8053ba6d
Sharmaz <irae45@gmail.com>
2015-10-25 21:31:35 -0600

    Ejemplo de server con middleware

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ var http = require('http').Server(app)
 
 var port = 8080
 
-app.get('/', function(req, res) {
-	res.send('<h1>Hello Wold!</h1>')
+// se ejecuta si se llama explicitamente en la definición de una ruta
+function middleHandler(req, res, next) {
+	console.log('Soy el middleware que anda por ahi')
+	next()
+}
+// se ejectua primero siempre
+app.use(function(req, res, next) {
+	console.log('Soy el 1er middleware')
+	next()
+})
+
+app.use(function(req, res, next) {
+	console.log('Soy el 2ndo middleware')
+	next()
+})
+
+app.get('/', middleHandler, function(req, res) {
+	//res.sendFile(__dirname + '/index.html')
+	console.log('Petición al Home')
 })
 
 http.listen(port, function(err) {
