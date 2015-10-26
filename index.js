30eb97c6e5f31eaa68abe165d4cb3162cc43e804
Sharmaz <irae45@gmail.com>
2015-10-26 03:17:10 -0600

    Se agregó funcionalidad al chat con Socket.io

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 var port = 8080
 
 app.use('/public', express.static(__dirname + '/public'))
-
-app.get('/', function(req, res) {
+app.use('*', function(req, res) {
 	res.sendFile(__dirname + '/index.html')
-	console.log('Petición al Home')
 })
 
-app.get('*', function (req, res) {
-	//retornar 404
+io.on('connection', function(socket) {
+	console.log('user connected')
+
+	socket.on('disconnect', function() {
+		console.log('user disconnected')
+	})
+
+})
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg)
+	})
+
 })
 
 http.listen(port, function(err) {
