2e1724f88cd4fd06dac069171a39f0e7fffd83e9
Sharmaz <irae45@gmail.com>
2015-10-25 21:42:02 -0600

    El server de la aplicación con nodejs usando el middleware static de express

--- index.js
+++ index.js
@@ -4,25 +4,15 @@ var http = require('http').Server(app)
 
 var port = 8080
 
-// se ejecuta si se llama explicitamente en la definición de una ruta
-function middleHandler(req, res, next) {
-	console.log('Soy el middleware que anda por ahi')
-	next()
-}
-// se ejectua primero siempre
-app.use(function(req, res, next) {
-	console.log('Soy el 1er middleware')
-	next()
-})
+app.use('/public', express.static(__dirname + '/public'))
 
-app.use(function(req, res, next) {
-	console.log('Soy el 2ndo middleware')
-	next()
-})
-
-app.get('/', middleHandler, function(req, res) {
-	//res.sendFile(__dirname + '/index.html')
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html')
 	console.log('Petición al Home')
+})
+
+app.get('*', function (req, res) {
+	//retornar 404
 })
 
 http.listen(port, function(err) {
